89253388a4b742ee0b11f29fd682bc4fccc11f15
hudson <hudsonb994@gmail.com>
2025-06-27 01:20:23 -0400

    Adicionado a funcionalidade de buscar participante, retornando seus dados.

--- participantes.py
+++ participantes.py
@@ -2,6 +2,21 @@ from util import *
  
 participantes = {}
 
+participantes["12345678900"] = {
+    "nome": "João Silva",
+    "cpf": "12345678900",
+    "idade": 30,
+    "email": "joao.silva@email.com",
+    "temas_preferidos": ["Python", "Data Science"]
+}
+
+participantes["98765432100"] = {
+    "nome": "Maria Souza",
+    "cpf": "98765432100",
+    "idade": 25,
+    "email": "maria.souza@email.com",
+    "temas_preferidos": ["JavaScript", "Web Development"]
+}
 '''
 {
     nome:
@@ -16,20 +31,27 @@ def menu_participantes():
         [cadastrar_participante, "Cadastrar participante"],
         [buscar_participante, "Buscar Participante"],
         [cadastrar_participante, "Cadastrar participantes"],
-        [cadastrar_participante, "Cadastrar participantes"]
+        [cadastrar_participante, "Cadastrar participantes"],
+        [None, "Voltar"]
     ]
 
     mostrar_menu(opcoes, "---- [ PARTICIPANTES ] ----")
     tratar_input(menu_participantes, opcoes, int(input("Escolha a Opção: ")))
 
 
-def cadastrar_participante():   
-    dados = form("Nome", "Cpf", "Idade", "Email", "Telefone")
-    participantes[dados["cpf"]] = dados 
-    return dados
-
+def cadastrar_participante():  
+    dados = form("nome", "cpf", "idade", "email", "telefone")
+    participantes[dados['cpf']] = dados
+    
 def buscar_participante():   
-    dados = form("Nome", "Cpf", "Idade", "Email", "Telefone")
-    participantes[dados["cpf"]] = dados 
-    return dados
- 
+    print("---- BUSCA PARTICIPANTE ----")
+    inp = input("Digite o cpf do participante: ")
+
+    if(participantes[inp]):
+        limpar_terminal()
+        print("---- DADOS ----")
+        mostrar_dados(participantes[inp])
+        return continuar()
+    
+    return buscar_participante()
+        
